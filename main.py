f32d1481ef04ce3c8b7068e69e7b2e7108d96f58
oksenyah <36799058+oksenyah@users.noreply.github.com>
2023-09-08 23:01:00 -0400

    minor updates

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-import requests
 import json
-import mysql.connector
+import sys
 from typing import List
+
+import mysql.connector
+import requests
+
 from card import Card
 
 
-HOSTNAME = "localhost"
+HOSTNAME = "127.0.0.1"
 USERNAME = "root"
+PASSWORD = "root"
 DBNAME = "YGOCards"
 MAIN_TABLE_NAME = "cards"
 
@@ -14,33 +18,45 @@ resp: requests.Response = requests.get("https://db.ygoprodeck.com/api/v7/cardinf
 all_card_data: List[dict] = json.loads(resp.text)["data"]
 cards: List[Card] = [Card(card_data) for card_data in all_card_data]
 
-serverConnection = mysql.connector.connect(
-    host=HOSTNAME,
-    user=USERNAME,
-)
-serverCursor = serverConnection.cursor()
+def main() -> int:
+    try:
+        with mysql.connector.connect(
+            host=HOSTNAME,
+            user=USERNAME,
+            password=PASSWORD,
+        ) as serverConnection:
+            serverCursor = serverConnection.cursor()
 
-serverCursor.execute("SHOW DATABASES")
-# cursor execution results are lists of tuples
-# in this case first item of each tuple is the name of a db
-if DBNAME not in [results[0] for results in serverCursor]:
-    # create the yu-gi-oh card db if it doesn't exist
-    serverCursor.execute(f"CREATE DATABASE {DBNAME}")
+            serverCursor.execute("SHOW DATABASES")
+            # cursor execution results are lists of tuples
+            # in this case first item of each tuple is the name of a db
+            if DBNAME.casefold() not in [results[0].casefold() for results in serverCursor]:
+                # create the yu-gi-oh card db if it doesn't exist
+                serverCursor.execute(f"CREATE DATABASE {DBNAME}")
 
-schemaConnection = mysql.connector.connect(
-    host=HOSTNAME,
-    user=USERNAME,
-    database=DBNAME
-)
-schemaCursor = schemaConnection.cursor()
+            with mysql.connector.connect(
+                host=HOSTNAME,
+                user=USERNAME,
+                password=PASSWORD,
+                database=DBNAME
+            ) as schemaConnection:
+                schemaCursor = schemaConnection.cursor()
 
-schemaCursor.execute("SHOW TABLES")
-if MAIN_TABLE_NAME not in [results[0] for results in schemaCursor]:
-    schemaCursor.execute(f"CREATE TABLE {MAIN_TABLE_NAME} (id VARCHAR(255), name VARCHAR(255))")
+                schemaCursor.execute("SHOW TABLES")
+                if MAIN_TABLE_NAME.casefold() not in [results[0].casefold() for results in schemaCursor]:
+                    schemaCursor.execute(f"CREATE TABLE {MAIN_TABLE_NAME} (id VARCHAR(255), name VARCHAR(255))")
 
-for card in cards:
-    schemaCursor.execute(f"INSERT INTO {MAIN_TABLE_NAME} (id, name) VALUES (%s, %s)", (card.id, card.name))
+                for card in cards:
+                    schemaCursor.execute(f"INSERT INTO {MAIN_TABLE_NAME} (id, name) VALUES (%s, %s)", (card.id, card.name))
 
-schemaConnection.commit()
+                schemaConnection.commit()
 
-print("done")
+            serverConnection.commit()
+    except mysql.connector.Error as ex:
+        print(f"ERROR: {ex}")
+        return 1
+
+    print("done")
+
+if __name__ == "__main__":
+    sys.exit(main())
